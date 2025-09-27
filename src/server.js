38fdfef2b0f78030be9cc21c80c5679d5f1d5c69
praveen <116637585+praveen-047@users.noreply.github.com>
2025-09-27 19:48:52 +0530

    'upgrading'

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 const app = require('./app');
 const dotenv = require('dotenv');
 dotenv.config();
+const cors = require("cors");
 
+app.use(cors());
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
